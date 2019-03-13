28d64e3b9b1499777b29a43f1413982cc4a43b63
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2019-03-13 15:45:19 +0100

    Clean up POC

--- src/test.php
+++ src/test.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-/** @psalm-var \Doctrine\Common\Collections\ArrayCollection<string> $collection */
+// Add the @var annotation to tell Psalm which type you want to enforce.
+/** @var \Doctrine\Common\Collections\ArrayCollection<string> $collection */
 $collection = new \Doctrine\Common\Collections\ArrayCollection();
+// Add a value of the correct type, no issues.
 $collection->add('test');
+// Try to add a value of a different type and Psalm will complain and throw an error! Profit?!
 $collection->add(new stdClass());
-
-/** @var int $returnVal */
-$returnVal = 5;
-
-/** @var TestClass<string> */
-$testClass = new TestClass('test');
-$returnVal = $testClass->getVal();
-
-$testClass->setVal($returnVal);
